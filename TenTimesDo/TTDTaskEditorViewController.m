a2e99ac811ec9111cb0aa1c5537a2041c47c49de
Jan Schulte <hello@unexpected-co.de>
2014-06-22 21:13:11 +0200

    Allow normal returns without any modifier in text field

--- TenTimesDo/TTDTaskEditorViewController.m
+++ TenTimesDo/TTDTaskEditorViewController.m
@@ -23,5 +23,34 @@
     [[self delegate] taskEditor:self didSaveTask:nil];
 }
 
+#pragma mark - TextView delegates
+
+/*
+From https://developer.apple.com/library/mac/qa/qa1454/_index.html
+
+This allows normal returns in multi line text fields without pressing an additional modifier key
+*/
+- (BOOL)control:(NSControl*)control textView:(NSTextView*)textView doCommandBySelector:(SEL)commandSelector
+{
+    BOOL result = NO;
+
+    if (commandSelector == @selector(insertNewline:))
+    {
+        // new line action:
+        // always insert a line-break character and don’t cause the receiver to end editing
+        [textView insertNewlineIgnoringFieldEditor:self];
+        result = YES;
+    }
+    else if (commandSelector == @selector(insertTab:))
+    {
+        // tab action:
+        // always insert a tab character and don’t cause the receiver to end editing
+        [textView insertTabIgnoringFieldEditor:self];
+        result = YES;
+    }
+
+    return result;
+}
+
 
 @end
